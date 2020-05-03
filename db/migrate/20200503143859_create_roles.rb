d819ed173b956618ad88ded0e377efa5347b4770
ElAtrash <bioelatrash@gmail.com>
2020-05-03 21:19:25 +0300

    fix columns names

--- db/migrate/20200503143859_create_roles.rb
+++ db/migrate/20200503143859_create_roles.rb
@@ -1,10 +1,10 @@
 class CreateRoles < ActiveRecord::Migration[6.0]
   def change
     create_table :roles do |t|
-      t.boolean :create, default: false
-      t.boolean :read, default: false
-      t.boolean :update, default: false
-      t.boolean :delete, default: false
+      t.boolean :can_create, default: false
+      t.boolean :can_read, default: false
+      t.boolean :can_update, default: false
+      t.boolean :can_delete, default: false
       t.boolean :admin, default: false
       t.string :role_type, null: false
       t.timestamps
