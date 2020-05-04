6e131db788ba5869d7b67d9de4b70b3736934df7
ElAtrash <bioelatrash@gmail.com>
2020-05-04 15:35:46 +0300

    add data to db

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'devise'
 # authorization
 gem 'pundit'
+# generate fake data
+gem 'faker'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,79 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning Database...'
+User.delete_all
+Subject.delete_all
+Role.delete_all
+
+puts 'Creating Roles...'
+admin = Role.create!(
+  can_read: true,
+  can_create: true,
+  can_update: true,
+  can_delete: true,
+  admin: true,
+  role_type: 'admin'
+)
+
+viewer = Role.create!(
+  can_read: true,
+  can_create: false,
+  can_update: false,
+  can_delete: false,
+  admin: false,
+  role_type: 'viewer'
+)
+
+puts 'Creating Users...'
+users_attributes = []
+10.times do 
+  user = {
+    username: Faker::Internet.unique.username,
+    email: Faker::Internet.unique.email,
+    password: Faker::Internet.password(min_length: 6),
+    role: viewer
+    }
+
+  users_attributes << user
+end 
+
+User.create!(users_attributes)
+
+admin_user = User.create!(
+  username: 'test_admin',
+  email: 'test@example.com',
+  password: 123456,
+  role: admin
+)
+
+puts 'Creating Subjects...'
+new_subjects_attributes = []
+10.times do 
+  new_subject = {
+    topic: Faker::Lorem.unique.sentence,
+    description: Faker::Lorem.unique.paragraph_by_chars(number: 256, supplemental: false),
+    status: 'New'
+    }
+
+  new_subjects_attributes << new_subject
+end 
+
+Subject.create!(new_subjects_attributes)
+
+old_subjects_attributes = []
+10.times do 
+  old_subject = {
+    topic: Faker::Lorem.unique.sentence,
+    description: Faker::Lorem.unique.paragraph_by_chars(number: 256, supplemental: false),
+    status: 'Old'
+    }
+
+  old_subjects_attributes << old_subject
+end 
+
+Subject.create!(old_subjects_attributes)
+
+puts 'Done!'
